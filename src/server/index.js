72bda74bcb67f7d298496783f85b9c701c0c0eca
dimitrov93 <ts.dimitrov93@gmail.com>
2023-02-17 15:51:24 +0200

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -11,7 +11,7 @@ const productRoute = require('./routes/product');
 const emailRoute = require('./routes/email');
 
 
-app.use(cors())
+app.use(cors(({origin: ["https://xoxostore-7ef9a.web.app", "http://localhost:3000", "http://localhost:4200"], credentials: true})))
 app.use(express.json())
 app.use(cookieParser());
 app.use(auth)
